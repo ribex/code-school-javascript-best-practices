8870734e4b9b1be94c8872d8dea7a48560fd3580
Rebecca Wagaman <ribexy@gmail.com>
2017-09-17 21:01:23 +0000

    Access is Granted complete

---
+++ mail-of-modularity/access-is-granted/cavesofclarity.js
@@ -0,0 +1,10 @@
+var CAVESOFCLARITY = {
+  stalactites: 4235,
+  stalagmites: 3924,
+  bats: 345,
+  treasureChests: 3,
+  openChest: function() {
+    this.treasureChests--;
+    alert('DA DADADA DAAAAAAA!');
+  }
+};
