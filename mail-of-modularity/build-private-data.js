2a1ff555cd3d8cd6064e1fc3a90da87d70f0011e
Rebecca Wagaman <ribexy@gmail.com>
2017-09-17 21:27:15 +0000

    Secure The Bats complete

--- mail-of-modularity/build-private-data.js
+++ mail-of-modularity/build-private-data.js
@@ -1,9 +1,9 @@
 var CAVESOFCLARITY = (function(){
   var treasureChests = 3;
+  var bats = 345;
   return{
     stalactites: 4235,
     stalagmites: 3924,
-    bats: 345,
     SECRET: {
       openChest: function() {
         treasureChests--;
