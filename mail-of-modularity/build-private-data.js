20ffd343389cc12fbdb6e4c3f7138717054f85f3
Rebecca Wagaman <ribexy@gmail.com>
2017-09-17 21:24:47 +0000

    Build Private Data complete

---
+++ mail-of-modularity/build-private-data.js
@@ -0,0 +1,14 @@
+var CAVESOFCLARITY = (function(){
+  var treasureChests = 3;
+  return{
+    stalactites: 4235,
+    stalagmites: 3924,
+    bats: 345,
+    SECRET: {
+      openChest: function() {
+        treasureChests--;
+        alert('DA DADADA DAAAAAAA!');
+      }
+    }
+  };
+})();
