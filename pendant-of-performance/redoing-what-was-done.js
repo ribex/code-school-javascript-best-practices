57f68a078f653adfd0cd5add53f89f2b1bc20543
Rebecca Wagaman <ribexy@gmail.com>
2017-09-17 13:23:11 +0000

    Redoing What Was Done - complete

---
+++ pendant-of-performance/redoing-what-was-done.js
@@ -0,0 +1,7 @@
+// Create a new invertedPeninsula
+var invertedPeninsula = new InvertedPeninsula();
+
+// Log the name of each invertedPeninsula inhabitant
+for (var i = 0; i < invertedPeninsula.inhabitants.length; i++) {
+  console.log(invertedPeninsula.inhabitants[i].name);
+}
