3cb144227e38741701e1ec888dbdd400a66c5381
Rebecca Wagaman <ribexy@gmail.com>
2017-09-17 21:06:59 +0000

    Treasure Chests complete

---
+++ mail-of-modularity/treasure-chests.js
@@ -0,0 +1,12 @@
+var CAVESOFCLARITY = {
+  stalactites: 4235,
+  stalagmites: 3924,
+  bats: 345,
+  SECRET : {
+    treasureChests: 3,
+    openChest: function() {
+      this.treasureChests--;
+      alert('DA DADADA DAAAAAAA!');
+    }
+  }
+};
